c3af00391f5356827415d8862cdab4ab99768c3e
swordy <mryangjian@live.com>
2015-08-13 11:00:26 +0800

    1. 动态显示产品

--- user/my.php
+++ user/my.php
@@ -24,8 +24,8 @@ require_once '_main.php'; ?>
                         <div class="box-body">
                             <p>用户名：<?php echo $U->GetUserName(); ?></p>
                             <p>邮箱：<?php echo $user_email; ?></p>
-                            <p> 套餐：<span class="label label-info"> <?php echo $oo->get_plan();?> </span> </p>
-                            <p> 账户余额：<?php echo $oo->get_money();?>元 </p>
+                            <p>套餐：<span class="label label-info"> <?php echo $oo->get_plan();?> </span> </p>
+                            <p>账户余额：<?php echo $oo->get_money();?>元 </p>
                             <!--<p><a class="btn btn-danger btn-sm" href="kill.php">删除我的账户</a></p>-->
                         </div><!-- /.box -->
                     </div>
@@ -45,18 +45,24 @@ require_once '_main.php'; ?>
             </h1>
         </section>
 
+        <?php 
+            $product = new \Ss\Product\Product();
+            $products = $product->AllProduct();
+        ?>
+
         <!-- Main content -->
         <section class="content">
             <div class="row">
+            <?php foreach ( $products as $p ){ ?>
                 <!-- left column -->
                 <div class="col-md-3">
                     <!-- general form elements -->
                     <div class="box box-primary">
                         <div class="box-body">
                             <form action="pay.php" method="post">
-                                <p>套餐名称：包月套餐</p>
-                                <p>价格：10￥</p>
-                                <p>说明：数量表示购买几个月</p>
+                                <p>套餐名称：<?php echo $p['name'] ?></p>
+                                <p>价格：<?php echo $p['price'] ?>￥</p>
+                                <p>说明：<?php echo $p['description'] ?></p>
                                 <p>数量：<input type="number" value="1" name="n"> </p>
                                 <input type="number" value="1" name="p" hidden="true">
                                 <input type="text" value="<?php (new \DateTime())->format('Y-m-d H:i:s') ?>" name="order_time" hidden="true">
@@ -65,41 +71,8 @@ require_once '_main.php'; ?>
                         </div><!-- /.box -->
                     </div>
                 </div>
-                <!-- left column -->
-                <div class="col-md-3">
-                    <!-- general form elements -->
-                    <div class="box box-primary">
-                        <div class="box-body">
-                            <form action="pay.php" method="post">
-                                <p>套餐名称：半年套餐</p>
-                                <p>价格：55￥</p>
-                                <p>说明：数量表示购买几个半年套餐</p>
-                                <p>数量：<input type="number" value="1" name="n"> </p>
-                                <input type="number" value="2" name="p" hidden="true">
-                                <input type="text" value="<?php (new \DateTime())->format('Y-m-d H:i:s') ?>" name="order_time" hidden="true">
-                                <input class="btn btn-danger btn-sm" type="submit" value="购买" />
-                            </form>
-                        </div><!-- /.box -->
-                    </div>
-                </div>
-                <!-- left column -->
-                <div class="col-md-3">
-                    <!-- general form elements -->
-                    <div class="box box-primary">
-                        <div class="box-body">
-                            <form action="pay.php" method="post">
-                                <p>套餐名称：包年套餐</p>
-                                <p>价格：100￥</p>
-                                <p>说明：数量表示购买多少年</p>
-                                <p>数量：<input type="number" value="1" name="n"> </p>
-                                <input type="number" value="3" name="p" hidden="true">
-                                <input type="text" value="<?php (new \DateTime())->format('Y-m-d H:i:s') ?>" name="order_time" hidden="true">
-                                <input class="btn btn-danger btn-sm" type="submit" value="购买" />
-                            </form>
-                        </div><!-- /.box -->
-                    </div>
-                </div>
-            </div>
+
+            <?php } ?>
         </section><!-- /.content -->
 
         <?php
